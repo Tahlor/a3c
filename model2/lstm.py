b9488ac13ede75e94179f3e2ab4638f7afbf0524
Ryan F. Lee <leery@ldschurch.org>
2018-03-27 16:30:46 -0600

    LSTM works now

--- model2/lstm.py
+++ model2/lstm.py
@@ -12,12 +12,13 @@ class LSTMCell(tf.nn.rnn_cell.RNNCell):
     def output_size(self):
         return self._state_size
 
-    def __call__(self, input, h_t_prev, c_t_prev, scope=None):
+    def __call__(self, inputs, state, scope=None):
 
-        combined_input = tf.concat([h_t_prev, input], axis=1)
+        # combine inputs before passing in
+        # combined_input = tf.concat([h_t_prev, input], axis=1)
 
         W_f = tf.get_variable(name='W_f',
-                              shape=(combined_input.shape[1], self._state_size),
+                              shape=(inputs.shape[1], self._state_size),
                               dtype=tf.float32,
                               initializer=tf.contrib.layers.variance_scaling_initializer())
 
@@ -27,7 +28,7 @@ class LSTMCell(tf.nn.rnn_cell.RNNCell):
                               initializer=tf.contrib.layers.variance_scaling_initializer())
 
         W_i = tf.get_variable(name='W_i',
-                              shape=(combined_input.shape[1], self._state_size),
+                              shape=(inputs.shape[1], self._state_size),
                               dtype=tf.float32,
                               initializer=tf.contrib.layers.variance_scaling_initializer())
 
@@ -37,7 +38,7 @@ class LSTMCell(tf.nn.rnn_cell.RNNCell):
                               initializer=tf.contrib.layers.variance_scaling_initializer())
 
         W_c = tf.get_variable(name='W_c',
-                              shape=(combined_input.shape[1], self._state_size),
+                              shape=(inputs.shape[1], self._state_size),
                               dtype=tf.float32,
                               initializer=tf.contrib.layers.variance_scaling_initializer())
 
@@ -47,7 +48,7 @@ class LSTMCell(tf.nn.rnn_cell.RNNCell):
                               initializer=tf.contrib.layers.variance_scaling_initializer())
 
         W_o = tf.get_variable(name='W_o',
-                              shape=(combined_input.shape[1], self._state_size),
+                              shape=(inputs.shape[1], self._state_size),
                               dtype=tf.float32,
                               initializer=tf.contrib.layers.variance_scaling_initializer())
 
@@ -56,12 +57,18 @@ class LSTMCell(tf.nn.rnn_cell.RNNCell):
                               dtype=tf.float32,
                               initializer=tf.contrib.layers.variance_scaling_initializer())
 
-        f_t = tf.sigmoid(tf.matmul(combined_input, W_f) + b_f)
-        i_t = tf.sigmoid(tf.matmul(combined_input, W_i) + b_i)
-        c_tilde_t = tf.tanh(tf.matmul(combined_input, W_c) + b_c)
+        # example vector shapes
+        # INPUT SHAPE = [1,10]
+        # HIDDEN SHAPE = [1,128]
+        #   COMBINED-INPUTS SHAPE = [1,138]
+        # CELL-STATE SHAPE = [1,128]
+        # [1,138] x [138,128] = [1,128]
+        f_t = tf.sigmoid(tf.matmul(inputs, W_f) + b_f)
+        i_t = tf.sigmoid(tf.matmul(inputs, W_i) + b_i)
+        c_tilde_t = tf.tanh(tf.matmul(inputs, W_c) + b_c)
 
-        c_t = (f_t * c_t_prev) + (i_t * c_tilde_t)
-        o_t = tf.sigmoid(tf.matmul(combined_input, W_o) + b_o)
+        c_t = (f_t * state) + (i_t * c_tilde_t)
+        o_t = tf.sigmoid(tf.matmul(inputs, W_o) + b_o)
         h_t = o_t * tf.tanh(c_t)
 
         return h_t, c_t
--- model2/test_lstm.py
+++ model2/test_lstm.py
@@ -8,18 +8,22 @@ STATE_SIZE = 25
 
 # build network
 lstm = LSTMCell(STATE_SIZE)
-src_ph = tf.placeholder(tf.float32, shape=[BATCH_SIZE, SEQUENCE_LENGTH], name='input')
-trg_ph = tf.placeholder(tf.float32, shape=[BATCH_SIZE, SEQUENCE_LENGTH], name='target')
+src_ph = tf.placeholder(tf.float32, shape=[BATCH_SIZE, SEQUENCE_LENGTH+STATE_SIZE], name='comb_inputs')
+# trg_ph = tf.placeholder(tf.float32, shape=[BATCH_SIZE, SEQUENCE_LENGTH], name='target')
 
 c_0 = tf.constant([1.], dtype=tf.float32, shape=[BATCH_SIZE, STATE_SIZE], name='c_0')
-h_0 = tf.constant([1.], dtype=tf.float32, shape=[BATCH_SIZE, SEQUENCE_LENGTH], name='h_0')
-h_1, c_1 = lstm(src_ph, h_0, c_0)
+# h_0 = tf.constant([1.], dtype=tf.float32, shape=[BATCH_SIZE, STATE_SIZE], name='h_0')
+states = lstm(src_ph, c_0)
 
-input = np.ones((BATCH_SIZE, SEQUENCE_LENGTH), dtype=tf.float32)
+input = np.ones((BATCH_SIZE, SEQUENCE_LENGTH), dtype=np.float32)
+h_0 = np.ones(shape=[BATCH_SIZE, STATE_SIZE], dtype=np.float32)
+comb_input = np.concatenate((h_0, input), axis=1)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 # summary_writer = tf.summary.FileWriter( "./tf_logs", graph=sess.graph )
 # saver = tf.train.Saver()
 
-sess.run(lstm, feed_dict={src_ph:input})
+h,c = sess.run(states, feed_dict={src_ph:comb_input})
+print("h: " + str(h))
+print("c: " + str(c))
