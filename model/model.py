57388eb427943300b7833bab4a2443e2fc3c7fec
Ryan F. Lee <leery@ldschurch.org>
2018-04-05 16:42:47 -0600

    Swapping out simple MLP for GRU

--- model/model.py
+++ model/model.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 import tensorflow.contrib.layers as tfcl
+from tensorflow.contrib.rnn import GRUCell
+import tensorflow.contrib.legacy_seq2seq as seq2seq
+import math
 import sys
 sys.path.append("..")
-import value
-import policy
+# import model.value
+# import model.policy
 
 def fc(inputs, num_nodes, name='0', activation=tf.nn.relu):
     weights = tf.get_variable('W_' + name,
@@ -19,10 +22,19 @@ def fc(inputs, num_nodes, name='0', activation=tf.nn.relu):
     net_value = tf.matmul(inputs, weights) + bias
     return activation(net_value)
 
+def get_gru(num_layers, state_dim, reuse=False):
+    with tf.variable_scope('gru', reuse=reuse):
+        gru_cells = []
+        for _ in range(num_layers):
+            gru_cells.append(GRUCell(state_dim))
+
+    return gru_cells
+
 
 class Model:
-    def __init__(self, input_size=10, layer_size=256, trainable = True, discount = .9):
+    def __init__(self, input_size=10, num_layers=1, layer_size=256, trainable = True, discount = .9, naive=False):
         self.input_size = input_size
+        self.num_layers = num_layers
         self.layer_size = layer_size
         self.inputs_ph = None
         self.targets_ph = None # useless for 'real' model, just here for the proof of concept
@@ -35,8 +47,8 @@ class Model:
         self.graph = tf.Graph()
         self.discount = discount
         self.entropy_weight = 1e-4
+        self.naive = naive
         self.build_network()
-
 
 
     def get_params(self):
@@ -46,9 +58,20 @@ class Model:
         with self.graph.as_default():
             self.inputs_ph = tf.placeholder(tf.float32, shape=[1, self.input_size], name='inputs')
             self.targets_ph = tf.placeholder(tf.float32, shape=[1], name='targets')
-            simple_network = fc(self.inputs_ph, self.layer_size)
 
-            output = simple_network
+            inputs = tf.split(self.inputs_ph, self.input_size, axis=1)
+
+            if self.naive:
+                network = fc(self.inputs_ph, self.layer_size)
+            else:
+                gru_cells = get_gru(self.num_layers, self.layer_size)
+                multi_cell = tf.nn.rnn_cell.MultiRNNCell(gru_cells)
+                initial_state = multi_cell.zero_state(batch_size=1, dtype=tf.float32)
+
+                with tf.variable_scope('rnn_decoder') as scope:
+                    outputs, final_state = seq2seq.rnn_decoder(inputs, initial_state, multi_cell)
+
+                network = final_state
 
             # Approach for a discrete action space, where we can either
             # buy or sell but don't specify an amount
@@ -61,8 +84,8 @@ class Model:
             # 0 means 'do nothing', and
             # 1 means 'buy everything you can'.
             # Exchange should know how to interpret this number.
-            self.actions_op = fc(output, 1, name='action', activation=tf.nn.tanh)
-            self.value_op = fc(output, 1, name='v')
+            self.actions_op = fc(network[0], 1, name='action', activation=tf.nn.tanh)
+            self.value_op = fc(network[0], 1, name='v')
             self.loss_op = tf.reduce_sum(self.targets_ph - self.actions_op, axis=1)
             self.optimizer = tf.train.RMSPropOptimizer(0.01).minimize(self.loss_op)
 
